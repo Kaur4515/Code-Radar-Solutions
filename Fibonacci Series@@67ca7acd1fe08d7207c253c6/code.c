b27e523d31cc00119ee7d43a107739329130f51c
Kaur4515 <agamjot0094.becse24@chitkara.edu.in>
2025-03-30 19:10:56 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int fibonacci(int a){
+    int count;
+    int set = 0 + 1; 
+    while(a){
+        count++;
+        a /= 10; 
+        int result = a + set;
+    }
+    return (result);
+}
+
+int main(){
+    int n; 
+    scanf("%d",&n);
+    if (n<=0){
+        printf("Invalid input\n");
+    return 0;
+    }
+     int done = fibonacci(n);
+    printf("%d",done);
+    return 0; 
+}
